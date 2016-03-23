62e6cd373283feb27aa57d1fbfee76971cf979b0
olivierloosvelt <olivier.loosvelt@hotmail.com>
2016-03-23 11:11:20 +0100

    get all data from specific card/all cards from database

--- Main.java
+++ Main.java
@@ -5,7 +5,8 @@ public class Main {
 	public static void main(String[] args) throws SQLException {
 		Game g1 = new Game();
 		DBConnection connect = new DBConnection();
-		connect.showResults(connect.getCardInfo("Smithy"));
+		//connect.showResults(connect.getCardInfo("Smithy"));
+		//connect.showResults(connect.getCardInfo());
 	}
 
 }
