ada38f69c8434fd2a8148e2ddb2d60815dce32b5
olivierloosvelt <olivier.loosvelt@hotmail.com>
2016-03-24 15:31:20 +0100

    database update

--- src/Model/DBConnection.java
+++ src/Model/DBConnection.java
@@ -6,7 +6,7 @@ import java.util.*;
 public class DBConnection {
 	private String url = "jdbc:mysql://185.13.227.167:3306/michiai147_domin";
 	private String user = "michiai147_domin";
-	private String pass = "projdominion2016";
+	private String pass = "";
 	private ResultSet resultset;
 	private Connection connection;
 	private PreparedStatement prepStatemnt;
